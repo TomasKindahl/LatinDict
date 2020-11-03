26a696a4c3968fb351f752515a4a8d285b6f563a
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-03 12:24:14 +0100

    Uppgift 6 klar

--- Program.cs
+++ Program.cs
@@ -21,7 +21,35 @@ namespace LatinDict
             {
                 Console.Write("> ");
                 command = Console.ReadLine();
-                Console.WriteLine("Läst kommando: {0}", command);
+                if (command == "visa")
+                {
+                    for (int i = 0; i < dict.Length; i++)
+                    {
+                        Console.WriteLine("{0}", dict[i]);
+                    }
+                }
+                else if (command == "svenska")
+                {
+                    Console.Write("Vilket ord vill du ha översatt: ");
+                    string lookUp = Console.ReadLine();
+                    for (int i = 0; i < dict.Length; i++)
+                    {
+                        string[] word = dict[i].Split('-');
+                        if (word[0] == lookUp)
+                        {
+                            Console.WriteLine("'{0}' är '{1}' på svenska", 
+                                word[0], word[1]);
+                        }
+                    }
+                }
+                else if (command == "sluta")
+                {
+                    Console.WriteLine("Adjö!");
+                }
+                else
+                {
+                    Console.WriteLine("Okänt kommando: {0}", command);
+                }
             } while (command != "sluta");
         }
     }
