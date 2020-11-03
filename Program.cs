e77b625b85153843c3465beaa69263504457430b
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-03 11:58:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till ordlistan!");
+            Console.WriteLine("Skriv 'sluta' för att sluta!");
+
+        }
+    }
+}
